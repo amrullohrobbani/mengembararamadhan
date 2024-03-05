916edb464876f401ce904923a99b0a73a174f3b8
amrullohrobbani <amrullohrobbani@gmail.com>
2024-03-05 16:41:23 +0700

    with aspectration

--- src/app/about-dev/page.js
+++ src/app/about-dev/page.js
@@ -5,30 +5,37 @@ import { AspectRatio } from "@/components/ui/aspect-ratio"
 export default function AboutDevPage() {
     return (
         <>
-            <div class="bg-black">
-                <div class="relative isolate px-6 pt-14 lg:px-8 grid grid-cols-2">
-                    <div className="w-full p-36">
-                        <AspectRatio ratio={3/5}>
-                            <Image src={imagePlaceholder} alt="PP" style={{clipPath: "inset(19% 70% 15% 16%),inset(8% 37% 4% 38%)"}}/>
+            <div className="bg-black h-auto md:w-screen">
+                <div className="relative isolate px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2">
+                    <div className="w-full px-36 relative">
+                        <AspectRatio ratio={3/5} className="relative">
+                            <div className="absolute w-full h-full bg-white" style={{clipPath: "inset(19% 65% 32% 23%)"}} />
+                            <Image src={imagePlaceholder} alt="PP" style={{clipPath: "inset(20% 66% 33% 24%)"}} className="absolute object-contain top-0" fill />
+                            <div className="absolute w-full h-full bg-white" style={{clipPath: "inset(6% 36% 23% 36%)"}} />
+                            <Image src={imagePlaceholder} alt="PP" style={{clipPath: "inset(6% 37% 24% 37%)"}} className="absolute object-contain top-0" fill />
+                            <div className="absolute w-full h-full bg-white -z-10" style={{clipPath: "inset(52% 31% 7% 50%)"}} />
+                            <Image src={imagePlaceholder} alt="PP" style={{clipPath: "inset(53% 32% 7% 51%)"}} className="absolute object-contain top-0 -z-10" fill />
+                            <Image src={imagePlaceholder} alt="PP" style={{clipPath: "inset(16% 27% 14% 71%)"}} className="absolute object-contain top-0" fill />
+                            <Image src={imagePlaceholder} alt="PP" style={{clipPath: "inset(23% 21% 22% 78%)"}} className="absolute object-contain top-0" fill />
                         </AspectRatio>
                     </div>
-                    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-                            <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                                Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+                    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                Announcing our next round of funding. <a href="#" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
                             </div>
                         </div>
-                        <div class="text-center">
-                            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
-                            <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-                            <div class="mt-10 flex items-center justify-center gap-x-6">
-                                <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-                                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+                        <div className="text-center">
+                            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
+                            <p className="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                            <div className="mt-10 flex items-center justify-center gap-x-6">
+                                <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+                                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
                             </div>
                         </div>
                     </div>
-                    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-                        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}}></div>
+                    <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+                        <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}}></div>
                     </div>
                 </div>
             </div>
